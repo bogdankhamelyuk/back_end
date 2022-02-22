73c1b5af58d75d3cc4ddda51a6b7118f32202c70
Bogdan Khamelyuk <bogdan.khamelyuk@sie.at>
2022-02-22 10:26:25 +0100

    changed allowed hosts

--- back_end/settings.py
+++ back_end/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-q7e*3rs@))-b3_$_+qz!$lvqf4ox*jt(y5f*1h65dk%ad3_$bt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['dialektsammler.azurewebsites.net']
 
 # Application definition
 
